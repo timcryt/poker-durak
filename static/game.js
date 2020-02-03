329b199f15901cb022ae9f3ac351f281e7039c1e
Тимофей <tymcrit@gmail.com>
2020-02-03 13:07:16 +0300

    Улучшено отображение карт

--- static/game.js
+++ static/game.js
@@ -1,4 +1,4 @@
-var socket = new WebSocket("ws://localhost:8000/ws", "echo");
+var socket = new WebSocket('ws://localhost:8000/ws', 'echo');
 
 function send(data) {{
     socket.send(data);
@@ -13,27 +13,35 @@ function refresh_state(data) {
     } else {
         data = data['Active']
         document.getElementById('state').innerText = 'Активное';
-        document.getElementById('comb').innerText = JSON.stringify(data['comb']['cards']);
-        document.getElementById('board').innerText = JSON.stringify(data['cards']);
+        document.getElementById('comb').innerText = print_cards(data['comb']['cards']);
+        document.getElementById('board').innerText = print_cards(data['cards']);
     }
+}
+
+function print_cards(cards) {
+    s = '';
+    cards.forEach(card => {
+        s += card['rank'] + ' ' + card['suit'] + '\n'
+    }); 
+    return s;
 }
 
 socket.onmessage = function(event) {{
     data = event.data;
-    if (data != "\"Pong\"") {
+    if (data != '"Pong"') {
         data = JSON.parse(data);
-        if (data["YourCards"]) {
-            document.getElementById('cards').innerText = JSON.stringify(data["YourCards"][0]);
-            document.getElementById('deck_size').innerText = JSON.stringify(data["YourCards"][1]);
-        } else if (data["YourTurn"]) {
+        if (data['YourCards']) {
+            document.getElementById('cards').innerText = print_cards(data['YourCards'][0]);
+            document.getElementById('deck_size').innerText = JSON.stringify(data['YourCards'][1]);
+        } else if (data['YourTurn']) {
             document.getElementById('your_turn').innerText = 'Да';
             document.getElementById('deck_size').innerText = JSON.stringify(data['YourTurn'][1]);
             refresh_state(data['YourTurn'][0]);
             
-        } else if (data["YouMadeStep"]) {
-            data = data["YouMadeStep"];
+        } else if (data['YouMadeStep']) {
+            data = data['YouMadeStep'];
             document.getElementById('your_turn').innerText = 'Нет';
-            document.getElementById('cards').innerText = JSON.stringify(data[1]);
+            document.getElementById('cards').innerText = print_cards(data[1]);
             document.getElementById('deck_size').innerText = JSON.stringify(data[2]);
             refresh_state(data[0]);
         }
@@ -41,5 +49,5 @@ socket.onmessage = function(event) {{
     }
 }}
 heartbit = function() {
-    send("\"Ping\"");
+    send('"Ping"');
 }
